c8c9922c50c46d092f9f01e38d92ee908a8eae44
n1stodev <nurillotojiakhmedov@gmail.com>
2023-05-12 22:03:43 +0500

    bitdi

--- src/router/single-route/SingleRoute.js
+++ src/router/single-route/SingleRoute.js
@@ -15,78 +15,80 @@ function SingleRoute() {
     const heart = useSelector(s => s.heart)
     const holat = true
     return (
-        <div className='single_route container'>
-            <div className="single-route__images">
-                <div className="sr__imagesLeft">
-                    <div className="sr__imagesLeftItem active">
+        <>
+            <div className='single_route container'>
+                <div className="single-route__images">
+                    <div className="sr__imagesLeft">
+                        <div className="sr__imagesLeftItem active">
+                            <img src={data?.images[0]} alt="" />
+                        </div>
+                        <div className="sr__imagesLeftItem">
+                            <img src={data?.images[1]} alt="" />
+                        </div>
+                        <div className="sr__imagesLeftItem">
+                            <img src={data?.images[2]} alt="" />
+                        </div>
+                    </div>
+                    <div className="sr__imagesRight">
                         <img src={data?.images[0]} alt="" />
-                    </div>
-                    <div className="sr__imagesLeftItem">
-                        <img src={data?.images[1]} alt="" />
-                    </div>
-                    <div className="sr__imagesLeftItem">
-                        <img src={data?.images[2]} alt="" />
-                    </div>
-                </div>
-                <div className="sr__imagesRight">
-                    <img src={data?.images[0]} alt="" />
-                    <div className="sr__contentTop">
-                        {
-                            heart?.some(i => i.id === data.id) ?
-                                <button onClick={() => dispatch(removeFromHeart(data))}>
-                                    <AiFillHeart />
-                                </button>
-                                :
-                                <button onClick={() => dispatch(addToHeart(data))}>
-                                    <AiOutlineHeart />
-                                </button>
-                        }
-                        <div />
+                        <div className="sr__contentTop">
+                            {
+                                heart?.some(i => i.id === data.id) ?
+                                    <button onClick={() => dispatch(removeFromHeart(data))}>
+                                        <AiFillHeart />
+                                    </button>
+                                    :
+                                    <button onClick={() => dispatch(addToHeart(data))}>
+                                        <AiOutlineHeart />
+                                    </button>
+                            }
+                            <div />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="single-route__content">
-                <h2>{data?.title}</h2>
-                <h1>{(data?.price).toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")} so'm</h1>
-                <div className="sr__content-comment">
-                    <AiFillStar />
-                    <AiFillStar />
-                    <AiFillStar />
-                    <AiFillStar />
-                    <AiFillStar />
-                    <span>0 ta sharx</span>
-                </div>
-                <p>Brend: {data?.title}</p>
-                <p className={`sr__content-holat ${holat ? 'active' : ''}`}>Holati: <span>- Sotuvda bor</span></p>
-                <div className="sr__content-share">
-                    <p>Uzatish: </p>
-                    <li>
-                        <FaFacebookF />
-                    </li>
-                    <li>
-                        <FaTelegramPlane />
-                    </li>
-                    <li>
-                        <FaTwitter />
-                    </li>
-                    <li>
-                        <FaWhatsapp />
-                    </li>
-                </div>
-                <div className="sr__content-btns">
-                    <button className='sr__content-btn'>
-                        <img src="https://asaxiy.uz/custom-assets/images/cart-white.svg" alt="" />
-                        <span>Savatchaga qo'shish</span>
-                    </button>
-                    <button className='sr__content-btn'>
-                        <span>Bir klikda olish</span>
-                    </button>
+                <div className="single-route__content">
+                    <h2>{data?.title}</h2>
+                    <h1>{(data?.price).toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")} so'm</h1>
+                    <div className="sr__content-comment">
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <span>0 ta sharx</span>
+                    </div>
+                    <p>Brend: {data?.title}</p>
+                    <p className={`sr__content-holat ${holat ? 'active' : ''}`}>Holati: <span>- Sotuvda bor</span></p>
+                    <div className="sr__content-share">
+                        <p>Uzatish: </p>
+                        <li>
+                            <FaFacebookF />
+                        </li>
+                        <li>
+                            <FaTelegramPlane />
+                        </li>
+                        <li>
+                            <FaTwitter />
+                        </li>
+                        <li>
+                            <FaWhatsapp />
+                        </li>
+                    </div>
+                    <div className="sr__content-btns">
+                        <button className='sr__content-btn'>
+                            <img src="https://asaxiy.uz/custom-assets/images/cart-white.svg" alt="" />
+                            <span>Savatchaga qo'shish</span>
+                        </button>
+                        <button className='sr__content-btn'>
+                            <span>Bir klikda olish</span>
+                        </button>
+                    </div>
                 </div>
             </div>
             <AboutApp />
             <LogoSwiper />
             <Comforts />
-        </div>
+        </>
     )
 }
 
